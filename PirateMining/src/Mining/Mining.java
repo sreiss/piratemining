b1c513a3f35eb46a3f8d6a7672eb22a21c3efb8b
Vincent Stébé <vincent.stebe@gmail.com>
2014-03-07 13:45:40 +0100

    Creating dataset

--- PirateMining/src/Mining/Mining.java
+++ PirateMining/src/Mining/Mining.java
@@ -1,19 +1,125 @@
 package Mining;
 
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import weka.classifiers.Classifier;
 import weka.classifiers.trees.J48;
+import weka.classifiers.trees.LMT;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
 import weka.core.Instances;
-import weka.filters.unsupervised.attribute.StringToWordVector;
+import weka.gui.treevisualizer.PlaceNode2;
+import weka.gui.treevisualizer.TreeVisualizer;
 
 public class Mining {
 	  /** The training data gathered so far. */
 	  private Instances _data = null;
 
 	  /** The filter used to generate the word counts. */
-	  private StringToWordVector _filter = new StringToWordVector();
+	  //private StringToWordVector _filter = new StringToWordVector();
 
 	  /** The actual classifier. */
 	  private Classifier _classifier = new J48();
 	  
+	  
+	  public void setPirates(ArrayList<Pirate> pirates)
+	  {
+		  
+		  FastVector attributes = new FastVector();
+		  
+		  String[] attributeNames = Pirate.getAttributeNames();
+		  
+		  for(String an : attributeNames)
+		  {
+			  FastVector att = new FastVector();
+			  att.addElement("true");
+			  att.addElement("false");
+			  attributes.addElement(new Attribute(an, att));
+		  }
+		  
+		  
+		  
+		 
+		     // 2. create Instances object
+		     _data = new Instances("MyRelation", attributes, 0);
+		     
+		     for(Pirate pirate : pirates)
+		     {System.out.println(pirate);
+		    	 double[] vals = new double[_data.numAttributes()];
+		    	 
+		    	 HashMap<String, Boolean> table = pirate.getTable();
+		    	 int i=0;
+		    	 
+		    	// Instance ins = new Instance();
+		    	 
+		    	 //ins.
+		    	 
+		    	 for(Entry<String, Boolean> entry : table.entrySet())
+		    	 {
+		    		 vals[i] = entry.getValue() ? 1 : 0;
+		    		 i++;
+		    	 }
+		    	 _data.add(new Instance(1.0, vals));
+		    	 _data.add(new Instance(1.0, vals));
+		     }
+		     
+		     _data.setClassIndex(_data.numAttributes() - 1); 
+	  }
+	  
+	  
+	  public void evaluate()
+	  {
+		  // train classifier
+		  Classifier cls = new J48();
+		  try {
+			cls.buildClassifier(_data);
+			
+
+		     // display classifier
+		     final javax.swing.JFrame jf = 
+		       new javax.swing.JFrame("Weka Classifier Tree Visualizer: J48");
+		     jf.setSize(500,400);
+		     jf.getContentPane().setLayout(new BorderLayout());
+		     TreeVisualizer tv = new TreeVisualizer(null,
+		         ((J48) cls).graph(),
+		         new PlaceNode2());
+		     jf.getContentPane().add(tv, BorderLayout.CENTER);
+		     jf.addWindowListener(new java.awt.event.WindowAdapter() {
+		       public void windowClosing(java.awt.event.WindowEvent e) {
+		         jf.dispose();
+		       }
+		     });
+		 
+		     jf.setVisible(true);
+		     tv.fitToScreen();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  // evaluate classifier and print some statistics
+		  //Evaluation eval = new Evaluation(_data);
+		  //eval.evaluateModel(cls, test);
+		  //System.out.println(eval.toSummaryString("\nResults\n======\n", false));
+	  }
+	  
+	  
+	  public static void main(String[] args)
+	  {
+		  ArrayList<Pirate> l = new ArrayList<Pirate>();
+		  Pirate p = new Pirate();
+		  p.setAttribute("bidule", true);
+		  l.add(p);
+		  p = new Pirate();
+		  l.add(p);
+		  p.setAttribute("bidule", false);
+		  
+		  Mining m = new Mining();
+		  m.setPirates(l);
+		  m.evaluate();
+	  }
 	  
 }
--- PirateMining/src/Mining/Pirate.java
+++ PirateMining/src/Mining/Pirate.java
@@ -4,14 +4,39 @@ import java.util.HashMap;
 
 public class Pirate {
 	private HashMap<String, Boolean> _attributes = new HashMap<String, Boolean>();
+	
+	private static final String[] attributesName = new String[]{"bidule", "truc"};
+	
+	public Pirate()
+	{
+		for(String s : attributesName)
+		{
+			_attributes.put(s, false);
+		}
+	}
 
 	public boolean hasAttribute(String name)
 	{
-		return _attributes.containsKey(name) && _attributes.get(name);
+		return _attributes.get(name);
+	}
+	
+	public HashMap<String, Boolean> getTable()
+	{
+		return _attributes;
 	}
 	
 	public void setAttribute(String name, boolean h)
 	{
 		_attributes.put(name, h);
+	}
+	
+	static public int countAttributes()
+	{
+		return attributesName.length;
+	}
+	
+	static public String[] getAttributeNames()
+	{
+		return attributesName;
 	}
 }
