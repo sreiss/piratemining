095cd3c1690a5dc5b1b742d771bebfc6cdf3df2e
bazinga27 <fredschmitt57@gmail.com>
2014-03-07 21:37:36 +0100

    buttons to checkboxes

--- PirateMining/src/gui/MainWindow.java
+++ PirateMining/src/gui/MainWindow.java
@@ -2,16 +2,22 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class MainWindow extends JFrame {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3596652768909677468L;
+	
 	private JPanel jpMain = new JPanel();
 	private JPanel jpBottom = new JPanel();
-//	private JButton jbValidate = new JButton("Valider");
-//	private JButton jbClose = new JButton("Fermer");
 	
 	public MainWindow() {
 		this.setLayout(new BorderLayout());
@@ -19,17 +25,35 @@ public class MainWindow extends JFrame {
 		JLabel jlPir = new JLabel("Ces pirates sont-ils dans la classe ?");
 		this.add(jlPir, BorderLayout.NORTH);
 		
-		jpMain.setLayout(new GridLayout());
+		jpMain.setLayout(new GridLayout(4,4));
 		
-		PirateButton pb = new PirateButton();
-		jpMain.add(pb);
+		for (int i = 0; i<16; i++) {
+			PirateButton pb = new PirateButton();
+			jpMain.add(pb);
+		}
 		
 		this.add(jpMain, BorderLayout.CENTER);
 		
-		this.setMinimumSize(new Dimension(300, 300));
-		this.setSize(300, 300);
+		jpBottom.setLayout(new FlowLayout());
+		
+		JButton jbValidate = new JButton("Valider");
+		JButton jbClose = new JButton("Fermer");
+		jbClose.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		jpBottom.add(jbValidate);
+		jpBottom.add(jbClose);
+
+		this.add(jpBottom, BorderLayout.SOUTH);
+		
+		
+		
+		this.setMinimumSize(new Dimension(750, 750));
+		this.setSize(750, 750);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
 	}
 
 	public static void main(String[] args) {
--- PirateMining/src/gui/PirateButton.java
+++ PirateMining/src/gui/PirateButton.java
@@ -1,23 +1,40 @@
 package gui;
 
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.ActionListener;
+import java.awt.Image;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
 
-public class PirateButton extends JButton {
+public class PirateButton extends JCheckBox {
 	
-	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2239474966130365554L;
+
 	public PirateButton() {
+		ImageIcon icon = new ImageIcon("D:\\Utilisateurs\\Fred\\Mes documents\\Divers\\Chrome-extension\\newtab\\img\\iutbleu.png");
+		Image img = icon.getImage();  
+		Image newimg = img.getScaledInstance(150, 150,  java.awt.Image.SCALE_SMOOTH);  
+		ImageIcon newIcon = new ImageIcon(newimg);
+		
+		this.setIcon(newIcon);
 		this.setBackground(Color.RED);
-		//this.addActionListener(new ActionListener());
+		this.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent arg0) {
+				changeBackground();
+			}
+		});
 	}
 	
 	public void changeBackground() {
-		if (this.getBackground() == Color.RED) {
+		if (this.isSelected() == true) {
 			this.setBackground(Color.GREEN);
-		} else if (this.getBackground() == Color.GREEN) {
+		} else {
 			this.setBackground(Color.RED);
 		}
 	}
