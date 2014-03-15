65cf5eeca17e1d9d07827a307363361c90047c8e
Vincent Stébé <vincent.stebe@gmail.com>
2014-03-15 23:55:02 +0100

    Nettoyage code Weka

--- PirateMining/src/mining/Mining.java
+++ PirateMining/src/mining/Mining.java
@@ -3,164 +3,105 @@ package mining;
 import java.awt.BorderLayout;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map.Entry;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.trees.J48;
-import weka.classifiers.trees.LMT;
 import weka.core.Attribute;
 import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.Debug.Random;
 import weka.gui.treevisualizer.PlaceNode2;
 import weka.gui.treevisualizer.TreeVisualizer;
 
 public class Mining {
-	  /** The training data gathered so far. */
-	  private Instances _data = null;
+	/** Les données Weka */
+	private Instances _data = null;
 
-	  /** The filter used to generate the word counts. */
-	  //private StringToWordVector _filter = new StringToWordVector();
 
-	  /** The actual classifier. */
-	  private Classifier _classifier = new J48();
-	  
-	  
-	  public void setPirates(ArrayList<Pirate> pirates)
-	  {
-		  
-		  FastVector attributes = new FastVector();
-		  
-		  String[] attributeNames = Pirate.getAttributeNames();
-		  
-		  for(String an : attributeNames)
-		  {
-			  FastVector att = new FastVector();
-			  att.addElement("true");
-			  att.addElement("false");
-			  attributes.addElement(new Attribute(an, att));
-		  }
-		  
-		  FastVector att = new FastVector();
-		  att.addElement("yes");
-		  att.addElement("no");
-		  attributes.addElement(new Attribute("ok", att, 0));
-		  
-		 
-		     // 2. create Instances object
-		     _data = new Instances("MyRelation", attributes, 0);
-		     _data.setClassIndex(_data.numAttributes() - 1); 
-		     for(Pirate pirate : pirates)
-		     {
-		    	 double[] vals = new double[_data.numAttributes()];
-		    	 
-		    	 HashMap<String, Boolean> table = pirate.getTable();
-		    	 int i=0;
-		    	 
-		    	// Instance ins = new Instance();
-		    	 
-		    	 //ins.
-		    	 
-		    	 for(String an : attributeNames)
-				  {
-		    		 vals[i] = table.get(an) ? 0 : 1;
-		    		 i++;
-		    	 }
-		    	 
-		    	 
-		    	 vals[vals.length -1] = pirate.isOk() ? 0 : 1;
-		    	 
-		    	 _data.add(new Instance(1.0, vals));
-		    	 //_data.add(new Instance(1.0, vals));
-		     }
-		     
-		    
-		     
-		     System.out.println(_data);
-	  }
-	  
-	  
-	  public void evaluate()
-	  {
-		  // train classifier
-		  Classifier cls = new J48();
-		  try {
-			cls.buildClassifier(_data);
+
+	public void setPirates(ArrayList<Pirate> pirates)
+	{
+		//Génération des attributs
+		FastVector attributes = new FastVector();
+
+		//On récupère le nom des attributs
+		String[] attributeNames = Pirate.getAttributeNames();
+
+		for(String an : attributeNames)
+		{
+			//On les ajoute à Weka
+			FastVector att = new FastVector();
+			att.addElement("true");
+			att.addElement("false");
+			attributes.addElement(new Attribute(an, att));
+		}
+
+		//Attribut final : pirate activé
+		FastVector att = new FastVector();
+		att.addElement("yes");
+		att.addElement("no");
+		attributes.addElement(new Attribute("ok", att, 0));
+
+
+		// 2. create Instances object
+		_data = new Instances("MyRelation", attributes, 0);
+		_data.setClassIndex(_data.numAttributes() - 1); 
+		for(Pirate pirate : pirates)
+		{
+			double[] vals = new double[_data.numAttributes()];
+
+			//La table des données, côté logiciel (non Weka)
+			HashMap<String, Boolean> table = pirate.getTable();
 			
-		     String graph = ((J48) cls).graph();
-		     
-		     String newGraph  = "";
-		     String[] lines = graph.split(System.getProperty("line.separator"));
-		     int i=0;
-		     for(String line : lines)
-		     {
-		    	 if(!line.contains("true") && !line.contains("false"))
-		    	 {
-		    		 String newLine;
-		    		 if(line.startsWith("N"))
-		    		 {
-		    			 newLine = "N" + i + " " + line.substring(line.indexOf(' '));
-		    			 i++;
-		    		 } else {
-		    			 newLine = line;
-		    		 }
-		    		 newGraph += newLine + "\n";
-		    	 }
-		     }
+			int i=0;
+			for(String an : attributeNames)
+			{
+				//Weka veut l'index de la valeur d'attribut à donner (ici 0 = "true", 1="false")
+				vals[i] = table.get(an) ? 0 : 1;
+				i++;
+			}
+
+			vals[vals.length -1] = pirate.isOk() ? 0 : 1;
+
+			_data.add(new Instance(1.0, vals)); //On ajoute à Weka
+		}
+
+		System.out.println(_data); //Debug, affichage ARFF
+	}
 
 
-		     
-	     
-		     
-		     
-		     
-		     
-		     
-		     
-		     // display classifier
-		     final javax.swing.JFrame jf = 
-		       new javax.swing.JFrame("Weka Classifier Tree Visualizer: J48");
-		     jf.setSize(500,400);
-		     jf.getContentPane().setLayout(new BorderLayout());
-		     TreeVisualizer tv = new TreeVisualizer(null,
-		         graph,
-		         new PlaceNode2());
-		     jf.getContentPane().add(tv, BorderLayout.CENTER);
-		     jf.addWindowListener(new java.awt.event.WindowAdapter() {
-		       public void windowClosing(java.awt.event.WindowEvent e) {
-		         jf.dispose();
-		       }
-		     });
+	public void evaluate()
+	{
+		//Création du classifier
+		Classifier cls = new J48();
+		try {
+			cls.buildClassifier(_data); //On lui donne les données
 
-		 
-		     jf.setVisible(true);
-		     tv.fitToScreen();
+			String graph = ((J48) cls).graph(); //Génération du graphique format text
+
+			//Affichage du graphique
+			final javax.swing.JFrame jf = 
+					new javax.swing.JFrame("Weka Classifier Tree Visualizer: J48");
+			jf.setSize(500,400);
+			jf.getContentPane().setLayout(new BorderLayout());
+			TreeVisualizer tv = new TreeVisualizer(null,
+					graph,
+					new PlaceNode2());
+			jf.getContentPane().add(tv, BorderLayout.CENTER);
+			jf.addWindowListener(new java.awt.event.WindowAdapter() {
+				public void windowClosing(java.awt.event.WindowEvent e) {
+					jf.dispose();
+				}
+			});
+
+
+			jf.setVisible(true);
+			tv.fitToScreen();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
-		  // evaluate classifier and print some statistics
-		  //Evaluation eval = new Evaluation(_data);
-		  //eval.evaluateModel(cls, test);
-		  //System.out.println(eval.toSummaryString("\nResults\n======\n", false));
-	  }
-	  
-	  
-	  public static void main(String[] args)
-	  {
-		  ArrayList<Pirate> liste = new ArrayList<Pirate>();
-		  Random r = new Random();
-		  for(int i=0; i<20; i++)
-		  {
-			  Pirate p = Pirate.randomPirate();
-			  p.setOk(r.nextInt(100) > 70 );
-			  liste.add(p);
-		  }
-		  
-		  Mining m = new Mining();
-		  m.setPirates(liste);
-		  m.evaluate();
-	  }
-	  
+
+	}
+
 }
